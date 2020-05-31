c100caf0f72e1ef0e2d9eac50f30f661e9a49cc1
XolaniMgube <xnmgube@gmail.com>
2020-05-31 22:55:01 +0200

    async func on post method, render returns index 1 of addNew

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 // requiring frameworks to use
+const Visitors = require('./table')
+
+
 let express = require('express');
 let bodyParser = require('body-parser');
+require('dotenv').config({path: '../.env'})
 let {Client} = require('pg')
+
+
 
 // postgres class that will start the connection
 const client = new Client({
-  user: "user",
-  password: "pass",
-  host: "localhost",
-  port: "5432",
-  database: "db"
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE
 })
 
 let app = express();//starting the program
@@ -24,33 +30,12 @@ app.get('/', function(req, res) {
 });
 
 // posting to new visitor path to render the html form data
-app.post('/new_visitor', urlencodedParser, function(req, res) { 
+app.post('/new_visitor', urlencodedParser, async function(req, res) { 
+  let table = new Visitors()
   
-    res.render('successful', {data: req.body});
-
-    // add new visitor function
-    async function addVisitor(name, age, date, time, assistedBy, comments) {
-      try{
-        await client.connect()
-        await client.query("BEGIN")
-        await client.query("insert into visitors (name, age, date, time, assistedby, comments) values ($1, $2, $3, $4, $5, $6)", [name, age, date, time, assistedBy, comments])
-        console.log("Inserted a new row")
-        let id = await client.query("select * from visitors where id = 1")
-        console.table(id.rows)
-        await client.query("COMMIT")
-        await client.end()
-      }
-      catch(ex){
-        console.log("Failed to add visitor " + ex)
-      }
-      finally{
-        
-        console.log("script closed")
-      }
-    }
-
+  
     // calling the addNewVisitor function
-    addVisitor(
+    let addNew = await table.addVisitor(
       req.body.name,
       req.body.age,
       req.body.date,
@@ -58,10 +43,18 @@ app.post('/new_visitor', urlencodedParser, function(req, res) {
       req.body.assistedby,
       req.body.comments
       )
+      console.log(addNew)
+      res.render('successful', 
+      {
+        data: addNew[0],
+      });
+
+      
+    
 });
 
-app.listen(3100, function() {
-  console.log('Listening on port 3100...')
+app.listen(3000, function() {
+  console.log('Listening on port 3000...')
 });
 
 
