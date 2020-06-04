7eac3f2bcb7b65b35b0f12f3295e49978f88922e
XolaniMgube <xnmgube@gmail.com>
2020-06-04 12:20:58 +0200

    clean code and added comments

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 // requiring frameworks to use
-const Visitors = require('./table')
+const express = require('express');
+const bodyParser = require('body-parser');
+const {Client} = require('pg')
 
-let express = require('express');
-let bodyParser = require('body-parser');
 require('dotenv').config({path: '../.env'})
-let {Client} = require('pg')
 
 // postgres class that will start the connection
 const client = new Client({
@@ -15,48 +14,41 @@ const client = new Client({
   database: process.env.PGDATABASE
 })
 
-let app = express();//starting the program
+// importing database queries
+const Visitors = require('./queries')
 
-let urlencodedParser = bodyParser.urlencoded({extended: false})//for viewing express on a pug template
+let app = express(); // starting the program
+let urlencodedParser = bodyParser.urlencoded({extended: false}) // for viewing express on a pug template
 
-app.set('view engine', 'pug');
-app.use('/css', express.static('css'))
+app.set('view engine', 'pug'); // setting pug as view engine
+app.use('/css', express.static('css')) // linking css to the html
 
-app.get('/', function(req, res) {
+// home page
+app.get('/new_visit', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 // posting to new visitor path to render the html form data
-app.post('/new_visitor', urlencodedParser, async function(req, res) { 
-  let table = new Visitors()
-
-    // creating table if it does not already exist
-    let checkTable = await table.createTable()
+app.post('/success', urlencodedParser, async (req, res) => { 
+  let visitorsTable = new Visitors() // creating an instance of the visitor table 
+  let newVisitorsTable = await visitorsTable.createTable() // this will create the visitor's table if it does not already exist
   
     // calling the addNewVisitor function
-    let addNew = await table.addVisitor(
+    let addNew = await visitorsTable.addVisitor(
       req.body.name,
       req.body.age,
       req.body.date,
       req.body.time,
       req.body.assistedby,
       req.body.comments
-      )
-
-      console.log(addNew)
+      );
       
-      res.render('successful', 
+      res.render('success.pug', 
       {
         data: addNew[0],
       });  
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log('Listening on port 3000...')
 });
-
-
-
-
-
-
